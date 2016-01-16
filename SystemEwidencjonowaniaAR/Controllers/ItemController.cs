322fe5215164f1373a5378938ea6508d1d21dcb7
tomekj <tomaszjankowski90@gmail.com>
2016-01-17 00:29:07 +0100

    pierwszy commit

---
+++ SystemEwidencjonowaniaAR/Controllers/ItemController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SystemEwidencjonowaniaAR.Context;
+using SystemEwidencjonowaniaAR.Models;
+
+namespace SystemEwidencjonowaniaAR.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ItemContext db = new ItemContext();
+        
+        // GET: Item
+        public ActionResult Index()
+        {
+            ViewBag.Index = "GET: Item";
+            return View(db.Items.ToList());
+        }
+
+        // GET: Item/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Item item = db.Items.Find(id);
+            if (item == null)
+                return HttpNotFound();
+            return View(item);
+        }
+
+        // GET: Item/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Item/Create
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Items.Add(item);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(item);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Item/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Item item = db.Items.Find(id);
+            if (item == null)
+                return HttpNotFound();
+            return View(item);
+        }
+
+        // POST: Item/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Item item)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                    
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Item/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Item/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SystemEwidencjonowaniaAR/Models/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SystemEwidencjonowaniaAR.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ID { get; set; }
+        public string KategoriaZasobu { get; set; }
+        [MaxLength(100) MinLength(5)]
+        public string Nazwa { get; set; }
+        [MaxLength(30)]
+        public string Producent { get; set; }
+        [MaxLength(50)]
+        public string Model { get; set; }
+        [MaxLength(250)]
+        public string Opis { get; set; }
+        [Index(IsUnique = true)]
+        [MaxLength(50)]
+        public string NrSeryjny { get; set; }
+        [Index(IsUnique = true) MaxLength(10)]
+        public string NrInw { get; set; }
+        [DefaultValue("nieznany")]
+        public string Status { get; set; }
+        public DateTime? DataWrpowadzenia { get; set; }
+    }
+}
