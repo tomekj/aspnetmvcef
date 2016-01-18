97d6108a812aa3164d990b901cb264b40dcfa9e7
tomekj <tomaszjankowski90@gmail.com>
2016-01-18 22:55:07 +0100

    start

---
+++ SystemEwidencjonowaniaAR/Controllers/CategoryController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SystemEwidencjonowaniaAR.Models;
+
+namespace SystemEwidencjonowaniaAR.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext db = ApplicationDbContext.Create();
+        // GET: KategriaZasobu
+        public ActionResult Index()
+        {
+            return View(db.Category.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Category kategoria = db.Category.Find(id);
+            ViewBag.Text = db.Category.ToList();
+            if (kategoria == null)
+                return HttpNotFound();
+            return View(kategoria);
+        }
+
+        // GET: Item/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Item/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Category")]Category kategoria)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Category.Add(kategoria);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(kategoria);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: KategriaZasobu/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: KategriaZasobu/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: KategriaZasobu/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: KategriaZasobu/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SystemEwidencjonowaniaAR/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SystemEwidencjonowaniaAR.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [MaxLength(100)]
+        public string Nazwa { get; set; }
+
+        public virtual ICollection<Item> Item { get; set; }
+    }
+}
